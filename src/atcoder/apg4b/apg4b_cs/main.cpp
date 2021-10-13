1700ba4441db1e0f47e42568a2524cc3a87abb22
Kazumasa Hirata <k_hirata@openwork.co.jp>
2021-10-13 14:41:50 +0900

    Add some answers

---
+++ src/atcoder/apg4b/apg4b_cs/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int one_year_in_seconds = 365 * 24 * 60 * 60;
+  int two_years_in_seconds = 2 * one_year_in_seconds;
+  int five_years_in_seconds = 5 * one_year_in_seconds;
+  int ten_years_in_seconds = 10 * one_year_in_seconds;
+
+  cout << one_year_in_seconds << endl;
+  cout << two_years_in_seconds << endl;
+  cout << five_years_in_seconds << endl;
+  cout << ten_years_in_seconds << endl;
+}
