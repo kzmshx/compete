4c1e3eb25fef9bd8cea0062e9aba9dfb49d6c78d
Kazumasa Hirata <k_hirata@openwork.co.jp>
2021-10-13 21:19:13 +0900

    Update command line tool

--- lib/lib.sh
+++ lib/lib.sh
@@ -41,30 +41,63 @@ build() {
   echo "$target"
 }
 
-assert() {
-  str_repeat() { seq -f "$1" -s "" "$2"; }
-  bar() { echo "$(str_repeat "$1" 100)"; }
-
+run_tests() {
   local red='\033[0;31m' green='\033[0;32m' nocolor='\033[0m'
-  local target="$1" in="$2" out="$3"
-  local actual result_diff result_status message
+  local target="$1" testcase_dir="$2"
 
-  actual="$(mktemp)"
-  trap "rm -f $actual" EXIT
-  $target <"$in" >"$actual"
+  cols() { tput cols; }
 
-  result_diff="$(sdiff "$out" "$actual")"
-  result_status=$?
-  if [ $result_status -eq 0 ]; then
-    message="${green}OK${nocolor}"
-  else
-    message="${red}NG${nocolor}"
-  fi
+  str_repeat() { seq -f "$1" -s "" "$2"; }
+
+  bar() { echo "$(str_repeat "$1" "$(cols)")"; }
+
+  assert() {
+    local target="$1" in="$2" out="$3"
+    local actual result_diff result_status message
+
+    actual="$(mktemp)"
+    trap "rm -f $actual" EXIT
+    $target <"$in" >"$actual"
+
+    result_diff="$(sdiff "$out" "$actual" -w "$(cols)")"
+    result_status=$?
+    if [ $result_status -eq 0 ]; then
+      message="${green}OK${nocolor}"
+    else
+      message="${red}NG${nocolor}"
+    fi
+
+    bar "-"
+    echo Input:
+    echo "$(cat $in)"
+    echo
+    echo Output:
+    echo $result_diff
+    echo
+    echo Status:
+    echo $message
+    bar "-"
+
+    return $result_status
+  }
+
+  in_file() { echo "$testcase_dir/sample-$1.in"; }
+
+  out_file() { echo "$testcase_dir/sample-$1.out"; }
+
+  local i=1 result_status n_ok=0 n_ng=0
+  while [ -f "$(in_file $i)" ] && [ -f "$(out_file $i)" ]; do
+    assert "$target" "$(in_file $i)" "$(out_file $i)"
+    if [ $? -eq 0 ]; then
+      n_ok=$((n_ok + 1))
+    else
+      n_ng=$((n_ng + 1))
+    fi
+    i=$((i + 1))
+  done
 
   bar "="
-  bar "-"
-  echo $result_diff
-  bar "-"
-  echo $message
+  [ $n_ok -gt 0 ] && echo "${green}${n_ok}${nocolor} passed."
+  [ $n_ng -gt 0 ] && echo "${red}${n_ng}${nocolor} failed."
   bar "="
 }
