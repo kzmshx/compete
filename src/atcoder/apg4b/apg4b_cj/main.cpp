dd1f46e0e4833f7e29c1f030ff2ea6d21e37882b
Kazumasa Hirata <k_hirata@openwork.co.jp>
2021-10-15 00:41:06 +0900

    Add a practice for vector

---
+++ src/atcoder/apg4b/apg4b_cj/main.cpp
@@ -0,0 +1,22 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int N;
+  cin >> N;
+
+  vector<int> A(N);
+  for (int i = 0; i < N; i++) {
+    cin >> A[i];
+  }
+
+  int sum = 0;
+  sum = accumulate(A.begin(), A.end(), sum);
+  int avg = sum / N;
+
+  for (int i = 0; i < N; i++) {
+    cout << abs(A[i] - avg) << endl;
+  }
+}
