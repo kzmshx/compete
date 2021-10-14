f1aff92c3f62465c63431f60a8ba375e712524ab
Kazumasa Hirata <k_hirata@openwork.co.jp>
2021-10-15 01:44:14 +0900

    Add some practices on arrays

---
+++ src/atcoder/abc085/abc085_b/main.cpp
@@ -0,0 +1,20 @@
+#define _GLIBCXX_DEBUG
+#include <bitset>
+#include <iostream>
+
+using namespace std;
+
+/**
+ * https://cpprefjp.github.io/reference/bitset/bitset.html
+ */
+int main() {
+  int N;
+  cin >> N;
+  bitset<101> D;
+  for (int i = 0; i < N; i++) {
+    int d;
+    cin >> d;
+    D.set(d);
+  }
+  cout << D.count() << endl;
+}
---
+++ src/atcoder/abc088/abc088_b/main.cpp
@@ -0,0 +1,32 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/**
+ * https://cpprefjp.github.io/reference/algorithm/sort.html
+ * https://cpprefjp.github.io/reference/functional/greater.html
+ */
+int main() {
+  int N;
+  cin >> N;
+
+  vector<int> A(N);
+  for (int i = 0; i < N; i++) {
+    cin >> A[i];
+  }
+
+  sort(A.begin(), A.end(), greater<>());
+
+  int a = 0;
+  int b = 0;
+  for (int i = 0; i < N; i++) {
+    if (i % 2 == 0) {
+      a += A[i];
+    } else {
+      b += A[i];
+    }
+  }
+
+  cout << a - b << endl;
+}
