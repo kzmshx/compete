28b189b44589c06d272c282feda6e3b5b438e50f
Kazumasa Hirata <k_hirata@openwork.co.jp>
2021-10-17 23:40:30 +0900

    Add a practice

---
+++ src/atcoder/abc062/abc062_b/main_0.cpp
@@ -0,0 +1,16 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int H, W;
+  cin >> H >> W;
+  cout << string(W + 2, '#') << endl;
+  for (int i = 0; i < H; i++) {
+    string a;
+    cin >> a;
+    cout << '#' << a << '#' << endl;
+  }
+  cout << string(W + 2, '#') << endl;
+}
