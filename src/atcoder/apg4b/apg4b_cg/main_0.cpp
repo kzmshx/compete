1c0b615af305d408cfa56822bbd93515d6b3959e
Kazumasa Hirata <k_hirata@openwork.co.jp>
2021-10-18 12:45:48 +0900

    Add a practice

---
+++ src/atcoder/apg4b/apg4b_cg/main_0.cpp
@@ -0,0 +1,18 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  vector<int> A(5);
+  cin >> A[0];
+  for (int i = 0; i < 4; i++) {
+    cin >> A[i + 1];
+    if (A[i] == A[i + 1]) {
+      cout << "YES" << endl;
+      return 0;
+    }
+  }
+
+  cout << "NO" << endl;
+}
