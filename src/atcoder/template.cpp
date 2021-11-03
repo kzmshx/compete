ba99be03d946f28c09fe829dc7f441d57cf90802
Kazumasa Hirata <k_hirata@openwork.co.jp>
2021-11-03 17:04:50 +0900

    Update C++ template

--- src/atcoder/template.cpp
+++ src/atcoder/template.cpp
@@ -2,5 +2,24 @@
 
 using namespace std;
 
+template<typename T> bool chmin(T &min, const T &value);
+template<typename T> bool chmax(T &max, const T &value);
+
 int main() {
+}
+
+template<typename T> bool chmin(T &min, const T &value) {
+    if (min > value) {
+        min = value;
+        return true;
+    }
+    return false;
+}
+
+template<typename T> bool chmax(T &max, const T &value) {
+    if (max < value) {
+        max = value;
+        return true;
+    }
+    return false;
 }
