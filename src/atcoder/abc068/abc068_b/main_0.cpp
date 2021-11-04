99c3c800a6f36ac856e17ba7220524df8bf191c6
Kazumasa Hirata <k_hirata@openwork.co.jp>
2021-11-04 17:50:29 +0900

    Solve abc068.b

---
+++ src/atcoder/abc068/abc068_b/main_0.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<typename T> bool choose_min(T &min, const T &value);
+template<typename T> bool choose_max(T &max, const T &value);
+
+int main() {
+    int N;
+    cin >> N;
+
+    int value = 1;
+    while (value * 2 <= N) {
+        value *= 2;
+    }
+
+    cout << value << endl;
+}
+
+template<typename T> bool choose_min(T &min, const T &value) {
+    if (min > value) {
+        min = value;
+        return true;
+    }
+    return false;
+}
+
+template<typename T> bool choose_max(T &max, const T &value) {
+    if (max < value) {
+        max = value;
+        return true;
+    }
+    return false;
+}
