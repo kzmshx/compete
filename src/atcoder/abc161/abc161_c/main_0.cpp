5454cb81cb9d31c8925f4dae3bc9cf72fff67609
Kazumasa Hirata <k_hirata@openwork.co.jp>
2021-11-05 08:47:43 +0900

    Solve abc161.c

---
+++ src/atcoder/abc161/abc161_c/main_0.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+template<typename T> bool choose_min(T &min, const T &value);
+template<typename T> bool choose_max(T &max, const T &value);
+
+int main() {
+    int64_t N, K;
+    cin >> N >> K;
+
+    int64_t mod = N % K;
+    cout << min(mod, K - mod) << endl;
+}
+
+template<typename T> bool choose_min(T &min, const T &value) {
+    if (min > value) {
+        min = value;
+        return true;
+    }
+    return false;
+}
+
+template<typename T> bool choose_max(T &max, const T &value) {
+    if (max < value) {
+        max = value;
+        return true;
+    }
+    return false;
+}
