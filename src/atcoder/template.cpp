7a7ae407e0d5207e07ecdae3a8367e0a7e862352
Kazumasa Hirata <k_hirata@openwork.co.jp>
2021-11-07 00:12:11 +0900

    C++ テンプレートに is_prime を追加

--- src/atcoder/template.cpp
+++ src/atcoder/template.cpp
@@ -2,13 +2,8 @@
 
 using namespace std;
 
-template<typename T> bool choose_min(T &min, const T &value);
-template<typename T> bool choose_max(T &max, const T &value);
-
-int main() {
-}
-
-template<typename T> bool choose_min(T &min, const T &value) {
+template<typename T>
+bool choose_min(T &min, const T &value) {
     if (min > value) {
         min = value;
         return true;
@@ -16,10 +11,30 @@ template<typename T> bool choose_min(T &min, const T &value) {
     return false;
 }
 
-template<typename T> bool choose_max(T &max, const T &value) {
+template<typename T>
+bool choose_max(T &max, const T &value) {
     if (max < value) {
         max = value;
         return true;
     }
     return false;
+}
+
+template<typename T, typename = enable_if_t<is_integral_v<T>>>
+bool is_prime(const T &integer) {
+    if (integer == 2) {
+        return true;
+    }
+    if (integer <= 1 || integer % 2 == 0) {
+        return false;
+    }
+    for (int v = 3; v <= sqrt(integer); v += 2) {
+        if (integer % v == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
 }
