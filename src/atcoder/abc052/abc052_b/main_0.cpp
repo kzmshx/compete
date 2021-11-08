e639c58d4bd513355808ba096db889649469eb40
Kazumasa Hirata <50480499+kzmsh@users.noreply.github.com>
2021-11-09 02:05:15 +0900

    Solve abc052.b

---
+++ src/atcoder/abc052/abc052_b/main_0.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<typename T>
+bool choose_min(T &min, const T &value) {
+    if (min > value) {
+        min = value;
+        return true;
+    }
+    return false;
+}
+
+template<typename T>
+bool choose_max(T &max, const T &value) {
+    if (max < value) {
+        max = value;
+        return true;
+    }
+    return false;
+}
+
+template<typename T, typename = enable_if_t<is_integral_v<T>>>
+bool is_prime(const T &integer) {
+    if (integer == 2) {
+        return true;
+    }
+    if (integer <= 1 || integer % 2 == 0) {
+        return false;
+    }
+    for (int v = 3; v <= sqrt(integer); v += 2) {
+        if (integer % v == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void run();
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    run();
+}
+
+void run() {
+    size_t N;
+    cin >> N;
+
+    string S;
+    cin >> S;
+
+    int max_sum = 0;
+    int current_sum = 0;
+    for (size_t i = 0; i < N; i++) {
+        if (S[i] == 'I') {
+            current_sum++;
+        } else {
+            current_sum--;
+        }
+        choose_max(max_sum, current_sum);
+    }
+
+    cout << max_sum << endl;
+}
