4a4bb7602ff9b943295480461a16c0b0c5ed57b8
Kazumasa Hirata <50480499+kzmsh@users.noreply.github.com>
2021-11-08 18:07:12 +0900

    Solve abc092.b

---
+++ src/atcoder/abc092/abc092_b/main_0.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<typename T>
+bool choose_min(T &min, const T &value) {
+    if (min > value) {
+        min = value;
+        return true;
+    }
+    return false;
+}
+
+template<typename T>
+bool choose_max(T &max, const T &value) {
+    if (max < value) {
+        max = value;
+        return true;
+    }
+    return false;
+}
+
+template<typename T, typename = enable_if_t<is_integral_v<T>>>
+bool is_prime(const T &integer) {
+    if (integer == 2) {
+        return true;
+    }
+    if (integer <= 1 || integer % 2 == 0) {
+        return false;
+    }
+    for (int v = 3; v <= sqrt(integer); v += 2) {
+        if (integer % v == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    int N, D, X;
+    cin >> N >> D >> X;
+
+    int initial_chocolate_count = N + X;
+    for (int i = 0; i < N; i++) {
+        int A;
+        cin >> A;
+
+        initial_chocolate_count += (D - 1) / A;
+    }
+
+    cout << initial_chocolate_count << endl;
+
+    return 0;
+}
