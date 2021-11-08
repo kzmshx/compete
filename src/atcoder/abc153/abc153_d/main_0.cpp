90d993658689cf397a7a2c8f83105a6e28a5b9e2
Kazumasa Hirata <50480499+kzmsh@users.noreply.github.com>
2021-11-08 20:05:10 +0900

    Solve abc153.d

---
+++ src/atcoder/abc153/abc153_d/main_0.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<typename T>
+bool choose_min(T &min, const T &value) {
+    if (min > value) {
+        min = value;
+        return true;
+    }
+    return false;
+}
+
+template<typename T>
+bool choose_max(T &max, const T &value) {
+    if (max < value) {
+        max = value;
+        return true;
+    }
+    return false;
+}
+
+template<typename T, typename = enable_if_t<is_integral_v<T>>>
+bool is_prime(const T &integer) {
+    if (integer == 2) {
+        return true;
+    }
+    if (integer <= 1 || integer % 2 == 0) {
+        return false;
+    }
+    for (int v = 3; v <= sqrt(integer); v += 2) {
+        if (integer % v == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void run();
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    run();
+}
+
+void run() {
+    int64_t H;
+    cin >> H;
+
+    int64_t attack_count = 1;
+    while (attack_count <= H) {
+        attack_count *= 2;
+    }
+
+    cout << attack_count - 1 << endl;
+}
