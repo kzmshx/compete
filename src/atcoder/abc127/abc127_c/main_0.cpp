ab99a91db7f00f5f20de6809848368e52fa22db0
Kazumasa Hirata <50480499+kzmsh@users.noreply.github.com>
2021-11-09 14:24:02 +0900

    Solve abc127.c

---
+++ src/atcoder/abc127/abc127_c/main_0.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<typename T>
+bool choose_min(T &min, const T &value) {
+    if (min > value) {
+        min = value;
+        return true;
+    }
+    return false;
+}
+
+template<typename T>
+bool choose_max(T &max, const T &value) {
+    if (max < value) {
+        max = value;
+        return true;
+    }
+    return false;
+}
+
+template<typename T, typename = enable_if_t<is_integral_v<T>>>
+bool is_prime(const T &integer) {
+    if (integer == 2) {
+        return true;
+    }
+    if (integer <= 1 || integer % 2 == 0) {
+        return false;
+    }
+    for (int v = 3; v <= sqrt(integer); v += 2) {
+        if (integer % v == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void run();
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    run();
+}
+
+void run() {
+    int N, M;
+    cin >> N >> M;
+
+    int max_L = 0, min_R = N;
+    for (int i = 0; i < M; i++) {
+        int L, R;
+        cin >> L >> R;
+        choose_max(max_L, L);
+        choose_min(min_R, R);
+    }
+
+    cout << max(min_R - max_L + 1, 0) << endl;
+}
