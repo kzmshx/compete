38607901cecc1c0093156aaff5c56248f72712f2
Kazumasa Hirata <50480499+kzmsh@users.noreply.github.com>
2021-11-14 07:38:38 +0900

    Solve abc104.b

---
+++ src/atcoder/abc104/abc104_b/main_0.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<typename T>
+bool choose_min(T &min, const T &value) {
+    if (min > value) {
+        min = value;
+        return true;
+    }
+    return false;
+}
+
+template<typename T>
+bool choose_max(T &max, const T &value) {
+    if (max < value) {
+        max = value;
+        return true;
+    }
+    return false;
+}
+
+template<typename T, typename = enable_if_t<is_integral_v<T>>>
+bool is_prime(const T &integer) {
+    if (integer == 2) {
+        return true;
+    }
+    if (integer <= 1 || integer % 2 == 0) {
+        return false;
+    }
+    for (int v = 3; v <= sqrt(integer); v += 2) {
+        if (integer % v == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void run();
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    run();
+}
+
+void run() {
+    string S;
+    cin >> S;
+
+    bool is_C_appeared = false;
+    for (size_t i = 0; i < S.size(); i++) {
+        if (i == 0) {
+            if (S[i] != 'A') {
+                cout << "WA" << endl;
+                return;
+            }
+        } else if (2 <= i && i <= S.size() - 2) {
+            if (S[i] == 'C') {
+                if (is_C_appeared) {
+                    cout << "WA" << endl;
+                    return;
+                }
+                is_C_appeared = true;
+            }
+        } else if (S[i] < 'a' || 'z' < S[i]) {
+            cout << "WA" << endl;
+            return;
+        }
+    }
+
+    if (is_C_appeared) {
+        cout << "AC" << endl;
+    } else {
+        cout << "WA" << endl;
+    }
+}
