a5537eef05439a2a080021a5fcd2610e3e9042f6
Kazumasa Hirata <50480499+kzmsh@users.noreply.github.com>
2021-11-14 15:39:50 +0900

    Solve abc053.b

---
+++ src/atcoder/abc053/abc053_b/main_0.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<typename T>
+bool choose_min(T &min, const T &value) {
+    if (min > value) {
+        min = value;
+        return true;
+    }
+    return false;
+}
+
+template<typename T>
+bool choose_max(T &max, const T &value) {
+    if (max < value) {
+        max = value;
+        return true;
+    }
+    return false;
+}
+
+template<typename T, typename = enable_if_t<is_integral_v<T>>>
+bool is_prime(const T &integer) {
+    if (integer == 2) {
+        return true;
+    }
+    if (integer <= 1 || integer % 2 == 0) {
+        return false;
+    }
+    for (int v = 3; v <= sqrt(integer); v += 2) {
+        if (integer % v == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void run();
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    run();
+}
+
+void run() {
+    string s;
+    cin >> s;
+    cout << max(int(s.find_last_of('Z')) - int(s.find_first_of('A')) + 1, 0) << endl;
+}
