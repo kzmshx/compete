830829d47875d6f7e30286521bbfb549b35a89c2
Kazumasa Hirata <50480499+kzmsh@users.noreply.github.com>
2021-11-17 21:35:24 +0900

    Solve abc136.c

---
+++ src/atcoder/abc136/abc136_c/main_0.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<typename T>
+bool choose_min(T &min, const T &value) {
+    if (min > value) {
+        min = value;
+        return true;
+    }
+    return false;
+}
+
+template<typename T>
+bool choose_max(T &max, const T &value) {
+    if (max < value) {
+        max = value;
+        return true;
+    }
+    return false;
+}
+
+template<typename T, typename = enable_if_t<is_integral_v<T>>>
+bool is_prime(const T &integer) {
+    if (integer == 2) {
+        return true;
+    }
+    if (integer <= 1 || integer % 2 == 0) {
+        return false;
+    }
+    for (int v = 3; v <= sqrt(integer); v += 2) {
+        if (integer % v == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void run();
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    run();
+}
+
+void run() {
+    int N;
+    cin >> N;
+
+    vector<int> A(N);
+    for (int i = 0; i < N; i++) {
+        cin >> A[i];
+    }
+
+    for (int i = N - 2; 0 <= i; i--) {
+        if (A[i] > A[i + 1] + 1) {
+            cout << "No" << endl;
+            return;
+        } else if (A[i] == A[i + 1] + 1) {
+            A[i]--;
+        }
+    }
+
+    cout << "Yes" << endl;
+}
