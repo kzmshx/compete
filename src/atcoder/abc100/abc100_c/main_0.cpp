7e0f501535e3fe58f95021a0a9f11d20927c89a9
Kazumasa Hirata <50480499+kzmsh@users.noreply.github.com>
2021-11-18 13:45:00 +0900

    Solve abc100.c

---
+++ src/atcoder/abc100/abc100_c/main_0.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<typename T>
+bool choose_min(T &min, const T &value) {
+    if (min > value) {
+        min = value;
+        return true;
+    }
+    return false;
+}
+
+template<typename T>
+bool choose_max(T &max, const T &value) {
+    if (max < value) {
+        max = value;
+        return true;
+    }
+    return false;
+}
+
+template<typename T, typename = enable_if_t<is_integral_v<T>>>
+bool is_prime(const T &integer) {
+    if (integer == 2) {
+        return true;
+    }
+    if (integer <= 1 || integer % 2 == 0) {
+        return false;
+    }
+    for (int v = 3; v <= sqrt(integer); v += 2) {
+        if (integer % v == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void run();
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    run();
+}
+
+void run() {
+    int N;
+    cin >> N;
+
+    int64_t count_of_two = 0;
+    for (int i = 0; i < N; i++) {
+        int64_t a;
+        cin >> a;
+
+        while (0 < a && a % 2 == 0) {
+            a /= 2;
+            count_of_two++;
+        }
+    }
+
+    cout << count_of_two << endl;
+}
