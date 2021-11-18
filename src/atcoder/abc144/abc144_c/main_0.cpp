099b648ee4c0eccd47ba1c2bea2dffe84ad21784
Kazumasa Hirata <50480499+kzmsh@users.noreply.github.com>
2021-11-18 10:58:10 +0900

    Solve abc144.c

---
+++ src/atcoder/abc144/abc144_c/main_0.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<typename T>
+bool choose_min(T &min, const T &value) {
+    if (min > value) {
+        min = value;
+        return true;
+    }
+    return false;
+}
+
+template<typename T>
+bool choose_max(T &max, const T &value) {
+    if (max < value) {
+        max = value;
+        return true;
+    }
+    return false;
+}
+
+template<typename T, typename = enable_if_t<is_integral_v<T>>>
+bool is_prime(const T &integer) {
+    if (integer == 2) {
+        return true;
+    }
+    if (integer <= 1 || integer % 2 == 0) {
+        return false;
+    }
+    for (int v = 3; v <= sqrt(integer); v += 2) {
+        if (integer % v == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void run();
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    run();
+}
+
+void run() {
+    int64_t N;
+    cin >> N;
+
+    int max_div = 1;
+    for (int64_t i = 2; i <= sqrt(N); i++) {
+        if (N % i == 0) {
+            max_div = i;
+        }
+    }
+
+    cout << max_div + N / max_div - 2 << endl;
+}
