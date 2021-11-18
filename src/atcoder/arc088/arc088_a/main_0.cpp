369d0548cf1c4d6c59e56690e51d349d5fe670e8
Kazumasa Hirata <50480499+kzmsh@users.noreply.github.com>
2021-11-19 01:45:20 +0900

    Solve arc088.a

---
+++ src/atcoder/arc088/arc088_a/main_0.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using i32 = int32_t;
+using i64 = int64_t;
+
+template<typename T>
+bool choose_min(T &min, const T &value) {
+    if (min > value) {
+        min = value;
+        return true;
+    }
+    return false;
+}
+
+template<typename T>
+bool choose_max(T &max, const T &value) {
+    if (max < value) {
+        max = value;
+        return true;
+    }
+    return false;
+}
+
+template<typename T, typename = enable_if_t<is_integral_v<T>>>
+bool is_prime(const T &integer) {
+    if (integer == 2) {
+        return true;
+    }
+    if (integer <= 1 || integer % 2 == 0) {
+        return false;
+    }
+    for (int v = 3; v <= sqrt(integer); v += 2) {
+        if (integer % v == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void run();
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    run();
+}
+
+void run() {
+    i64 X, Y;
+    cin >> X >> Y;
+    int c = 0;
+    while (X <= Y) {
+        X *= 2;
+        c++;
+    }
+    cout << c << endl;
+}
