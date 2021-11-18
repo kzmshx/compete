ffe1bd0d4ba71174ab3fe7ce0739f985ef34b42e
Kazumasa Hirata <50480499+kzmsh@users.noreply.github.com>
2021-11-18 12:53:41 +0900

    Solve agc041.a

---
+++ src/atcoder/agc041/agc041_a/main_0.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<typename T>
+bool choose_min(T &min, const T &value) {
+    if (min > value) {
+        min = value;
+        return true;
+    }
+    return false;
+}
+
+template<typename T>
+bool choose_max(T &max, const T &value) {
+    if (max < value) {
+        max = value;
+        return true;
+    }
+    return false;
+}
+
+template<typename T, typename = enable_if_t<is_integral_v<T>>>
+bool is_prime(const T &integer) {
+    if (integer == 2) {
+        return true;
+    }
+    if (integer <= 1 || integer % 2 == 0) {
+        return false;
+    }
+    for (int v = 3; v <= sqrt(integer); v += 2) {
+        if (integer % v == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void run();
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    run();
+}
+
+void run() {
+    int64_t N, A, B;
+    cin >> N >> A >> B;
+    cout << ((B - A) % 2 == 0 ? (B - A) / 2 : min((A + B - 1) / 2, (2 * N - A - B + 1) / 2)) << endl;
+}
