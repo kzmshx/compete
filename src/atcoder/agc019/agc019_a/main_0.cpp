6a610355a651519a55030cee97b9d63e4b100625
Kazumasa Hirata <50480499+kzmsh@users.noreply.github.com>
2021-11-18 16:53:55 +0900

    Solve agc019.a

---
+++ src/atcoder/agc019/agc019_a/main_0.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<typename T>
+bool choose_min(T &min, const T &value) {
+    if (min > value) {
+        min = value;
+        return true;
+    }
+    return false;
+}
+
+template<typename T>
+bool choose_max(T &max, const T &value) {
+    if (max < value) {
+        max = value;
+        return true;
+    }
+    return false;
+}
+
+template<typename T, typename = enable_if_t<is_integral_v<T>>>
+bool is_prime(const T &integer) {
+    if (integer == 2) {
+        return true;
+    }
+    if (integer <= 1 || integer % 2 == 0) {
+        return false;
+    }
+    for (int v = 3; v <= sqrt(integer); v += 2) {
+        if (integer % v == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void run();
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    run();
+}
+
+void run() {
+    int64_t Q, H, S, D, N;
+    cin >> Q >> H >> S >> D >> N;
+    int64_t min_per_liter = min(min(4 * Q, 2 * H), S);
+    int64_t min_per_two_liter = min(2 * min_per_liter, D);
+    cout << min_per_two_liter * (N / 2) + min_per_liter * (N % 2) << endl;
+}
