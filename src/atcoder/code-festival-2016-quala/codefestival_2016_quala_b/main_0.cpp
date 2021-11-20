04dbec3243566116a1af361e02877841275f035b
Kazumasa Hirata <50480499+kzmsh@users.noreply.github.com>
2021-11-20 11:20:36 +0900

    Solve codefestival_2016_quala.b

---
+++ src/atcoder/code-festival-2016-quala/codefestival_2016_quala_b/main_0.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using i32 = int32_t;
+using i64 = int64_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+using f32 = float;
+using f64 = double;
+
+template<typename T>
+bool choose_min(T &min, const T &value) {
+    if (min > value) {
+        min = value;
+        return true;
+    }
+    return false;
+}
+
+template<typename T>
+bool choose_max(T &max, const T &value) {
+    if (max < value) {
+        max = value;
+        return true;
+    }
+    return false;
+}
+
+template<typename T, typename = enable_if_t<is_integral_v<T>>>
+bool is_prime(const T &integer) {
+    if (integer == 2) {
+        return true;
+    }
+    if (integer <= 1 || integer % 2 == 0) {
+        return false;
+    }
+    for (int v = 3; v <= sqrt(integer); v += 2) {
+        if (integer % v == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void run();
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    run();
+}
+
+void run() {
+    i32 N;
+    cin >> N;
+
+    vector<i32> rabbits(N);
+    for (i32 i = 0; i < N; i++) {
+        i32 v;
+        cin >> v;
+        rabbits[i] = v - 1;
+    }
+
+    i32 answer = 0;
+    for (i32 i = 0; i < N - 1; i++) {
+        if (i < rabbits[i] && i == rabbits[rabbits[i]]) {
+            answer++;
+        }
+    }
+
+    cout << answer << endl;
+}
