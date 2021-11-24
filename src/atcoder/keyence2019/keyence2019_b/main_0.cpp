ea5a1f7828451bb009a025fee962d11764190e78
Kazumasa Hirata <50480499+kzmsh@users.noreply.github.com>
2021-11-24 21:30:28 +0900

    Solve keyence_2019.b

---
+++ src/atcoder/keyence2019/keyence2019_b/main_0.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using i32 = int32_t;
+using i64 = int64_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+using f32 = float;
+using f64 = double;
+
+template<typename T>
+bool choose_min(T &min, const T &value) {
+    if (min > value) {
+        min = value;
+        return true;
+    }
+    return false;
+}
+
+template<typename T>
+bool choose_max(T &max, const T &value) {
+    if (max < value) {
+        max = value;
+        return true;
+    }
+    return false;
+}
+
+template<typename T, typename = enable_if_t<is_integral_v<T>>>
+bool is_prime(const T &integer) {
+    if (integer == 2) {
+        return true;
+    }
+    if (integer <= 1 || integer % 2 == 0) {
+        return false;
+    }
+    for (int v = 3; v <= sqrt(integer); v += 2) {
+        if (integer % v == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void run();
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    run();
+}
+
+void run() {
+    string S;
+    cin >> S;
+
+    string keyence = "keyence";
+    for (i32 i = 0; i < (i32) S.size(); i++) {
+        string current_substr = (S.size() <= i + keyence.size() ? S.substr(0, i + keyence.size() - S.size()) : "") + S.substr(i, keyence.size());
+        if (current_substr == keyence) {
+            cout << "YES" << endl;
+            return;
+        }
+    }
+
+    cout << "NO" << endl;
+}
