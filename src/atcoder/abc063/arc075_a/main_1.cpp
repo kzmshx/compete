f9ef1bb1ddc412e14aa24f9c01cf323cfb5b739e
Kazumasa Hirata <50480499+kzmsh@users.noreply.github.com>
2021-11-25 01:19:14 +0900

    Solve arc075.a

---
+++ src/atcoder/abc063/arc075_a/main_1.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using i32 = int32_t;
+using i64 = int64_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+using f32 = float;
+using f64 = double;
+
+template<typename T>
+bool choose_min(T &min, const T &value) {
+    if (min > value) {
+        min = value;
+        return true;
+    }
+    return false;
+}
+
+template<typename T>
+bool choose_max(T &max, const T &value) {
+    if (max < value) {
+        max = value;
+        return true;
+    }
+    return false;
+}
+
+template<typename T, typename = enable_if_t<is_integral_v<T>>>
+bool is_prime(const T &integer) {
+    if (integer == 2) {
+        return true;
+    }
+    if (integer <= 1 || integer % 2 == 0) {
+        return false;
+    }
+    for (int v = 3; v <= sqrt(integer); v += 2) {
+        if (integer % v == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void run();
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    run();
+}
+
+i32 solve(vector<i32> &s) {
+    sort(s.begin(), s.end());
+
+    i32 sum = accumulate(s.begin(), s.end(), 0);
+    i32 first_not_divisible_by_ten = sum;
+    for (const auto &v : s) {
+        if (v % 10 != 0) {
+            first_not_divisible_by_ten = v;
+            break;
+        }
+    }
+
+    return sum % 10 == 0 ? sum - first_not_divisible_by_ten : sum;
+}
+
+void run() {
+    i32 N;
+    cin >> N;
+
+    vector<i32> s(N);
+    for (auto &v : s) {
+        cin >> v;
+    }
+
+    cout << solve(s) << endl;
+}
