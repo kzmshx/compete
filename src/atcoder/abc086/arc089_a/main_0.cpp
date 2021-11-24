5c318b4f21627f18b5e6e20e360f1fb801cee74b
Kazumasa Hirata <50480499+kzmsh@users.noreply.github.com>
2021-11-24 13:55:46 +0900

    Solve arc089.a

---
+++ src/atcoder/abc086/arc089_a/main_0.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using i32 = int32_t;
+using i64 = int64_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+using f32 = float;
+using f64 = double;
+
+template<typename T>
+bool choose_min(T &min, const T &value) {
+    if (min > value) {
+        min = value;
+        return true;
+    }
+    return false;
+}
+
+template<typename T>
+bool choose_max(T &max, const T &value) {
+    if (max < value) {
+        max = value;
+        return true;
+    }
+    return false;
+}
+
+template<typename T, typename = enable_if_t<is_integral_v<T>>>
+bool is_prime(const T &integer) {
+    if (integer == 2) {
+        return true;
+    }
+    if (integer <= 1 || integer % 2 == 0) {
+        return false;
+    }
+    for (int v = 3; v <= sqrt(integer); v += 2) {
+        if (integer % v == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void run();
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    run();
+}
+
+void run() {
+    i32 N;
+    cin >> N;
+
+    i32 t = 0, x = 0, y = 0;
+    for (i32 i = 0; i < N; i++) {
+        i32 ti, xi, yi;
+        cin >> ti >> xi >> yi;
+
+        if (abs(xi - x) + abs(yi - y) <= ti - t && (abs(xi - x) + abs(yi - y) + ti - t) % 2 == 0) {
+            t = ti, x = xi, y = yi;
+            continue;
+        }
+
+        cout << "No" << endl;
+        return;
+    }
+
+    cout << "Yes" << endl;
+}
