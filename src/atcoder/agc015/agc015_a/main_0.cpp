84edeefd3988b34b4029c44873e5eb250a1f52eb
Kazumasa Hirata <50480499+kzmsh@users.noreply.github.com>
2021-11-28 15:42:20 +0900

    Solve agc015.a

---
+++ src/atcoder/agc015/agc015_a/main_0.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using i32 = int32_t;
+using i64 = int64_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+using f32 = float;
+using f64 = double;
+
+template<typename T>
+bool choose_min(T &min, const T &value) {
+    if (min > value) {
+        min = value;
+        return true;
+    }
+    return false;
+}
+
+template<typename T>
+bool choose_max(T &max, const T &value) {
+    if (max < value) {
+        max = value;
+        return true;
+    }
+    return false;
+}
+
+template<typename T, typename = enable_if_t<is_integral_v<T>>>
+bool is_prime(const T &integer) {
+    if (integer == 2) {
+        return true;
+    }
+    if (integer <= 1 || integer % 2 == 0) {
+        return false;
+    }
+    for (int v = 3; v <= sqrt(integer); v += 2) {
+        if (integer % v == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void run();
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    run();
+}
+
+i64 solve(i64 N, i64 A, i64 B) {
+    if (B < A || (N == 1 && A != B)) {
+        return 0;
+    }
+    return (A + B * (N - 1)) - (A * (N - 1) + B) + 1;
+}
+
+void run() {
+    i64 N, A, B;
+    cin >> N >> A >> B;
+    cout << solve(N, A, B) << endl;
+}
