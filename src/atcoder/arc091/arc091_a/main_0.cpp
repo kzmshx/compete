6fdde12ee47475acc9c899f809ba8eb5e901d13b
Kazumasa Hirata <50480499+kzmsh@users.noreply.github.com>
2021-11-29 01:59:43 +0900

    Solve arc091.a

---
+++ src/atcoder/arc091/arc091_a/main_0.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using i32 = int32_t;
+using i64 = int64_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+using f32 = float;
+using f64 = double;
+
+template<typename T>
+bool choose_min(T &min, const T &value) {
+    if (min > value) {
+        min = value;
+        return true;
+    }
+    return false;
+}
+
+template<typename T>
+bool choose_max(T &max, const T &value) {
+    if (max < value) {
+        max = value;
+        return true;
+    }
+    return false;
+}
+
+template<typename T, typename = enable_if_t<is_integral_v<T>>>
+bool is_prime(const T &integer) {
+    if (integer == 2) {
+        return true;
+    }
+    if (integer <= 1 || integer % 2 == 0) {
+        return false;
+    }
+    for (int v = 3; v <= sqrt(integer); v += 2) {
+        if (integer % v == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void run();
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    run();
+}
+
+i64 solve(i64 N, i64 M) {
+    if (N == 1 && M == 1) {
+        return 1;
+    } else if (N == 1) {
+        return M - 2;
+    } else if (M == 1) {
+        return N - 2;
+    } else {
+        return (N - 2) * (M - 2);
+    }
+}
+
+void run() {
+    i64 N, M;
+    cin >> N >> M;
+    cout << solve(N, M) << endl;
+}
