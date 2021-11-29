4d22add538c8bf61295596d214b753be12af21fe
Kazumasa Hirata <50480499+kzmsh@users.noreply.github.com>
2021-11-29 22:48:24 +0900

    Solve abc043.b

---
+++ src/atcoder/abc043/abc043_b/main_0.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using i32 = int32_t;
+using i64 = int64_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+using f32 = float;
+using f64 = double;
+
+template<typename T>
+bool choose_min(T &min, const T &value) {
+    if (min > value) {
+        min = value;
+        return true;
+    }
+    return false;
+}
+
+template<typename T>
+bool choose_max(T &max, const T &value) {
+    if (max < value) {
+        max = value;
+        return true;
+    }
+    return false;
+}
+
+template<typename T, typename = enable_if_t<is_integral_v<T>>>
+bool is_prime(const T &integer) {
+    if (integer == 2) {
+        return true;
+    }
+    if (integer <= 1 || integer % 2 == 0) {
+        return false;
+    }
+    for (int v = 3; v <= sqrt(integer); v += 2) {
+        if (integer % v == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void run();
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    run();
+}
+
+string solve(string s) {
+    vector<char> buffer(0);
+    for (const auto &c : s) {
+        if (c == 'B') {
+            if (buffer.size() > 0) {
+                buffer.pop_back();
+            }
+            continue;
+        }
+
+        buffer.push_back(c);
+    }
+    return string(buffer.begin(), buffer.end());
+}
+
+void run() {
+    string s;
+    cin >> s;
+    cout << solve(s) << endl;
+}
