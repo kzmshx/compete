623d7d672aa0029baf41a31e80591edc59837b47
Kazumasa Hirata <50480499+kzmsh@users.noreply.github.com>
2021-11-30 21:58:47 +0900

    Solve abc109.c

---
+++ src/atcoder/abc109/abc109_c/main_0.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using i32 = int32_t;
+using i64 = int64_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+using f32 = float;
+using f64 = double;
+
+template<typename T>
+bool choose_min(T &min, const T &value) {
+    if (min > value) {
+        min = value;
+        return true;
+    }
+    return false;
+}
+
+template<typename T>
+bool choose_max(T &max, const T &value) {
+    if (max < value) {
+        max = value;
+        return true;
+    }
+    return false;
+}
+
+template<typename T, typename = enable_if_t<is_integral_v<T>>>
+bool is_prime(const T &integer) {
+    if (integer == 2) {
+        return true;
+    }
+    if (integer <= 1 || integer % 2 == 0) {
+        return false;
+    }
+    for (int v = 3; v <= sqrt(integer); v += 2) {
+        if (integer % v == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void run();
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    run();
+}
+
+i64 solve(vector<i64> locations, i64 X) {
+    i32 GCD = abs(locations[0] - X);
+    for (i64 i = 1; i < (i64) locations.size(); i++) {
+        GCD = gcd(GCD, abs(locations[i] - X));
+    }
+    return GCD;
+}
+
+void run() {
+    i64 N, X;
+    cin >> N >> X;
+
+    vector<i64> locations(N);
+    for (auto &location : locations) {
+        cin >> location;
+    }
+
+    cout << solve(locations, X) << endl;
+}
