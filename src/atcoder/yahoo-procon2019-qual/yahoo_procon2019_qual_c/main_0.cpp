283a5d137c4438cf701ddb5c90b67933a5a0c527
Kazumasa Hirata <50480499+kzmsh@users.noreply.github.com>
2021-12-02 01:24:10 +0900

    Solve yahoo_procon2019_qual.c

---
+++ src/atcoder/yahoo-procon2019-qual/yahoo_procon2019_qual_c/main_0.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using i32 = int32_t;
+using i64 = int64_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+using f32 = float;
+using f64 = double;
+
+template<typename T>
+bool choose_min(T &min, const T &value) {
+    if (min > value) {
+        min = value;
+        return true;
+    }
+    return false;
+}
+
+template<typename T>
+bool choose_max(T &max, const T &value) {
+    if (max < value) {
+        max = value;
+        return true;
+    }
+    return false;
+}
+
+template<typename T, typename = enable_if_t<is_integral_v<T>>>
+bool is_prime(const T &integer) {
+    if (integer == 2) {
+        return true;
+    }
+    if (integer <= 1 || integer % 2 == 0) {
+        return false;
+    }
+    for (int v = 3; v <= sqrt(integer); v += 2) {
+        if (integer % v == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void run();
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    run();
+}
+
+i64 solve(i64 K, i64 A, i64 B) {
+    if (K <= A - 1 || B - A <= 2) {
+        return K + 1;
+    }
+    K -= A - 1;
+    return A + ((K / 2) * (B - A)) + K % 2;
+}
+
+void run() {
+    i64 K, A, B;
+    cin >> K >> A >> B;
+    cout << solve(K, A, B) << endl;
+}
