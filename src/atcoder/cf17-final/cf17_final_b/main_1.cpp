01dd324061ec4fb2a8e0005229a3a87144319f6d
Kazumasa Hirata <50480499+kzmsh@users.noreply.github.com>
2021-12-06 20:41:19 +0900

    Solve cf17_final.b

---
+++ src/atcoder/cf17-final/cf17_final_b/main_1.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using i32 = int32_t;
+using i64 = int64_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+using f32 = float;
+using f64 = double;
+
+template<typename T>
+bool choose_min(T &min, const T &value) {
+    if (min > value) {
+        min = value;
+        return true;
+    }
+    return false;
+}
+
+template<typename T>
+bool choose_max(T &max, const T &value) {
+    if (max < value) {
+        max = value;
+        return true;
+    }
+    return false;
+}
+
+template<typename T, typename = enable_if_t<is_integral_v<T>>>
+bool is_prime(const T &integer) {
+    if (integer == 2) {
+        return true;
+    }
+    if (integer <= 1 || integer % 2 == 0) {
+        return false;
+    }
+    for (int v = 3; v <= sqrt(integer); v += 2) {
+        if (integer % v == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void run();
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    run();
+}
+
+bool solve(string S) {
+    i32 a = count(S.begin(), S.end(), 'a'), b = count(S.begin(), S.end(), 'b'), c = count(S.begin(), S.end(), 'c');
+    return abs(a - b) < 2 && abs(b - c) < 2 && abs(c - a) < 2;
+}
+
+void run() {
+    string S;
+    cin >> S;
+    cout << (solve(S) ? "YES" : "NO") << endl;
+}
