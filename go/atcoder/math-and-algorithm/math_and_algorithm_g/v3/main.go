ade3f0f871384ee04093ffad1f402e763564c802
Kazumasa Hirata <k_hirata@openwork.co.jp>
2023-11-23 21:26:23 +0900

    go/atcoder/math-and-algorithm/math_and_algorithm_g

--- go/atcoder/math-and-algorithm/math_and_algorithm_g/v3/main.go
+++ go/atcoder/math-and-algorithm/math_and_algorithm_g/v3/main.go
@@ -4,54 +4,27 @@ import (
 	"fmt"
 )
 
-// Input returns a value.
-func Input[T any]() T {
+// input returns a value.
+func input[T any]() T {
 	var value T
 	fmt.Scan(&value)
 	return value
 }
 
-// Inputs returns n values.
-func Inputs[T any](n int) []T {
-	values := make([]T, n)
-	for i := 0; i < n; i++ {
-		values[i] = Input[T]()
-	}
-	return values
-}
-
-// Addable is the type of values that support addition.
-type Addable interface {
-	int | int8 | int16 | int32 | int64 |
-		uint | uint8 | uint16 | uint32 | uint64 | uintptr |
-		float32 | float64 |
-		complex64 | complex128 |
-		string
-}
-
-// Sum returns the sum of values.
-func Sum[T Addable](values []T) T {
-	var sum T
-	for _, value := range values {
-		sum += value
-	}
-	return sum
-}
-
-// GCD returns the greatest common divisor of a and b.
-func GCD(a int, b int) int {
+// gcd returns the greatest common divisor of a and b.
+func gcd(a int, b int) int {
 	for b != 0 {
 		a, b = b, a%b
 	}
 	return a
 }
 
-// LCM returns the least common multiple of a and b.
-func LCM(a int, b int) int {
-	return a * b / GCD(a, b)
+// lcm returns the least common multiple of a and b.
+func lcm(a int, b int) int {
+	return a * b / gcd(a, b)
 }
 
 func main() {
-	n, x, y := Input[int](), Input[int](), Input[int]()
-	fmt.Println(n/x + n/y - n/LCM(x, y))
+	n, x, y := input[int](), input[int](), input[int]()
+	fmt.Println(n/x + n/y - n/lcm(x, y))
 }
