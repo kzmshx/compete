5ec64cc37e5bf3facfd76d01c7b1c5a26223b036
Kazumasa Hirata <k_hirata@openwork.co.jp>
2023-11-23 15:44:55 +0900

    go/atcoder/math-and-algorithm/math_and_algorithm_g

---
+++ go/atcoder/math-and-algorithm/math_and_algorithm_g/v1/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+)
+
+func Input[T any]() T {
+	var value T
+	fmt.Scan(&value)
+	return value
+}
+
+func Inputs[T any](n int) []T {
+	values := make([]T, n)
+	for i := 0; i < n; i++ {
+		values[i] = Input[T]()
+	}
+	return values
+}
+
+type Addable interface {
+	int | int8 | int16 | int32 | int64 |
+		uint | uint8 | uint16 | uint32 | uint64 | uintptr |
+		float32 | float64 |
+		complex64 | complex128 |
+		string
+}
+
+func Sum[T Addable](values []T) T {
+	var sum T
+	for _, value := range values {
+		sum += value
+	}
+	return sum
+}
+
+func main() {
+	n, x, y := Input[int](), Input[int](), Input[int]()
+	c := 0
+	for i := 1; i <= n; i++ {
+		if i%x == 0 || i%y == 0 {
+			c++
+		}
+	}
+	fmt.Println(c)
+}
