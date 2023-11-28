6d611ae187611b69f7d93849e21fd64e7a8cd5d3
Kazumasa Hirata <k_hirata@openwork.co.jp>
2023-11-28 23:24:22 +0900

    https://atcoder.jp/contests/dp/tasks/dp_e

---
+++ go/atcoder/dp/dp_e/v1/main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	N, W := read[int](), read[int]()
+	ws, vs, vSum := make([]int, N), make([]int, N), 0
+	for i := 0; i < N; i++ {
+		ws[i], vs[i] = read[int](), read[int]()
+		vSum += vs[i]
+	}
+
+	dp := sliceFunc(N+1, func(i int) []int { return sliceFill(vSum+1, math.MaxInt32) })
+	dp[0][0] = 0
+	for i := 0; i < N; i++ {
+		wi, vi := ws[i], vs[i]
+		for v := 0; v <= vSum; v++ {
+			chmin(&dp[i+1][v], dp[i][v])
+			if v+vi <= vSum {
+				chmin(&dp[i+1][v+vi], dp[i][v]+wi)
+			}
+		}
+	}
+
+	for i := vSum; i >= 0; i-- {
+		if dp[N][i] <= W {
+			fmt.Println(i)
+			return
+		}
+	}
+}
+
+// signed is a constraint that permits any signed integer type.
+type signed interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64
+}
+
+// unsigned is a constraint that permits any unsigned integer type.
+type unsigned interface {
+	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr
+}
+
+// integer is a constraint that permits any integer type.
+type integer interface {
+	signed | unsigned
+}
+
+// float is a constraint that permits any floating-point type.
+type float interface {
+	~float32 | ~float64
+}
+
+// ordered is a constraint that permits any ordered type: any type
+type ordered interface {
+	integer | float | ~string
+}
+
+// read reads a value from stdin.
+func read[T any]() (r T) {
+	fmt.Scan(&r)
+	return r
+}
+
+// chmin sets the minimum value of a and b to a and returns the minimum value.
+func chmin[T ordered](a *T, b T) T {
+	if *a > b {
+		*a = b
+	}
+	return *a
+}
+
+// sliceFill returns a slice of length n with each element set to v.
+func sliceFill[T any](n int, v T) []T {
+	r := make([]T, n)
+	for i := 0; i < n; i++ {
+		r[i] = v
+	}
+	return r
+}
+
+// sliceFunc returns a slice of length n with each element set to the result of f(i).
+func sliceFunc[T any](n int, f func(int) T) []T {
+	r := make([]T, n)
+	for i := 0; i < n; i++ {
+		r[i] = f(i)
+	}
+	return r
+}
