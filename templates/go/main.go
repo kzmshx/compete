3991d90880784ce54999acc5f9d93fa41234e532
Kazumasa Hirata <k_hirata@openwork.co.jp>
2023-12-09 17:21:36 +0900

    Update templates/go/main.go

--- templates/go/main.go
+++ templates/go/main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"os"
+	"strconv"
 )
 
 func main() {
-	fmt.Println("Hello, world!")
+	r := NewScanner(os.Stdin, MaxBufferSize)
+	w := NewWriter(os.Stdout)
+	defer w.Flush()
+
+	w.Println(r.Int())
 }
 
 // signed is a constraint that permits any signed integer type.
@@ -48,19 +56,58 @@ type addable interface {
 	integer | float | imaginary | ~string
 }
 
-// read reads a value from stdin.
-func read[T any]() (r T) {
-	fmt.Scan(&r)
-	return r
+const MaxBufferSize = 1 * 1024 * 1024
+
+type Scanner struct {
+	sc *bufio.Scanner
 }
 
-// readSlice reads n values from stdin.
-func readSlice[T any](n int) []T {
-	r := make([]T, n)
-	for i := 0; i < n; i++ {
-		r[i] = read[T]()
+func NewScanner(r io.Reader, size int) *Scanner {
+	sc := bufio.NewScanner(r)
+	sc.Buffer(make([]byte, size), size)
+	sc.Split(bufio.ScanWords)
+	return &Scanner{sc}
+}
+
+func (s *Scanner) String() string {
+	s.sc.Scan()
+	return s.sc.Text()
+}
+
+func (s *Scanner) Int() int {
+	n, err := strconv.Atoi(s.String())
+	if err != nil {
+		panic(err)
 	}
-	return r
+	return n
+}
+
+func (s *Scanner) Float64() float64 {
+	n, err := strconv.ParseFloat(s.String(), 64)
+	if err != nil {
+		panic(err)
+	}
+	return n
+}
+
+type Writer struct {
+	bf *bufio.Writer
+}
+
+func NewWriter(w io.Writer) *Writer {
+	return &Writer{bufio.NewWriter(w)}
+}
+
+func (w *Writer) Print(a ...interface{}) {
+	fmt.Fprint(w.bf, a...)
+}
+
+func (w *Writer) Println(a ...interface{}) {
+	fmt.Fprintln(w.bf, a...)
+}
+
+func (w *Writer) Flush() {
+	w.bf.Flush()
 }
 
 // chmax sets the maximum value of a and b to a and returns the maximum value.
@@ -150,15 +197,15 @@ func lcm(a, b int) int {
 	return a * b / gcd(a, b)
 }
 
-// unionFind is a disjoint-set data structure.
-type unionFind struct {
+// UnionFind is a disjoint-set data structure.
+type UnionFind struct {
 	parent []int // parent[i] = parent of i
 	size   []int // size[i] = number of elements in subtree rooted at i
 }
 
-// newUnionFind creates a new union-find data structure with n elements.
-func newUnionFind(n int) *unionFind {
-	u := &unionFind{
+// NewUnionFind creates a new union-find data structure with n elements.
+func NewUnionFind(n int) *UnionFind {
+	u := &UnionFind{
 		parent: make([]int, n),
 		size:   make([]int, n),
 	}
@@ -170,7 +217,7 @@ func newUnionFind(n int) *unionFind {
 }
 
 // Root returns the root of the component that element x belongs to.
-func (u *unionFind) Root(x int) int {
+func (u *UnionFind) Root(x int) int {
 	// x is the root of the tree
 	if u.parent[x] == -1 {
 		return x
@@ -182,12 +229,12 @@ func (u *unionFind) Root(x int) int {
 }
 
 // IsSameSet returns true if elements x and y belong to the same component.
-func (u *unionFind) IsSameSet(x, y int) bool {
+func (u *UnionFind) IsSameSet(x, y int) bool {
 	return u.Root(x) == u.Root(y)
 }
 
 // Union merges the components that elements x and y belong to.
-func (u *unionFind) Union(x, y int) bool {
+func (u *UnionFind) Union(x, y int) bool {
 	xRoot, yRoot := u.Root(x), u.Root(y)
 	if xRoot == yRoot {
 		return false
@@ -204,6 +251,6 @@ func (u *unionFind) Union(x, y int) bool {
 }
 
 // Size returns the size of the component that element x belongs to.
-func (u *unionFind) Size(x int) int {
+func (u *UnionFind) Size(x int) int {
 	return u.size[u.Root(x)]
 }
