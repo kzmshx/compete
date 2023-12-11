d69494010747c7ddc555947b8584e3492cc36be5
Kazumasa Hirata <k_hirata@openwork.co.jp>
2023-12-12 02:14:38 +0900

    https://atcoder.jp/contests/abc044/tasks/abc044_a

---
+++ go/atcoder/abc044/abc044_a/v1/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+func Solve(r *Scanner, w *Writer) {
+	N, K, X, Y := r.Int(), r.Int(), r.Int(), r.Int()
+	w.Println(X*min(N, K) + Y*max(0, N-K))
+}
+
+func main() {
+	r, w := NewScanner(os.Stdin, MaxBufferSize), NewWriter(os.Stdout)
+	defer w.Flush()
+	Solve(r, w)
+}
+
+// signed is a constraint that permits any signed integer type.
+type signed interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64
+}
+
+// unsigned is a constraint that permits any unsigned integer type.
+type unsigned interface {
+	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr
+}
+
+// integer is a constraint that permits any integer type.
+type integer interface {
+	signed | unsigned
+}
+
+// float is a constraint that permits any floating-point type.
+type float interface {
+	~float32 | ~float64
+}
+
+// ordered is a constraint that permits any ordered type: any type
+type ordered interface {
+	integer | float | ~string
+}
+
+const MaxBufferSize = 1 * 1024 * 1024
+
+type Scanner struct{ sc *bufio.Scanner }
+
+func NewScanner(r io.Reader, size int) *Scanner {
+	sc := bufio.NewScanner(r)
+	sc.Buffer(make([]byte, size), size)
+	sc.Split(bufio.ScanWords)
+	return &Scanner{sc}
+}
+func (s *Scanner) scan() bool       { return s.sc.Scan() }
+func (s *Scanner) text() string     { return s.sc.Text() }
+func (s *Scanner) String() string   { s.scan(); return s.text() }
+func (s *Scanner) Int() int         { return atoi(s.String()) }
+func (s *Scanner) Float64() float64 { return atof(s.String()) }
+
+type Writer struct{ bf *bufio.Writer }
+
+func NewWriter(w io.Writer) *Writer        { return &Writer{bufio.NewWriter(w)} }
+func (w *Writer) Print(a ...interface{})   { fmt.Fprint(w.bf, a...) }
+func (w *Writer) Println(a ...interface{}) { fmt.Fprintln(w.bf, a...) }
+func (w *Writer) Flush()                   { w.bf.Flush() }
+
+// unwrap returns the value of v if err is nil and panics otherwise.
+func unwrap[T any](v T, err error) T {
+	if err != nil {
+		panic(err)
+	}
+	return v
+}
+
+// atoi returns an integer converted from s.
+func atoi(s string) int { return unwrap(strconv.Atoi(s)) }
+
+// atof returns a float converted from s.
+func atof(s string) float64 { return unwrap(strconv.ParseFloat(s, 64)) }
+
+// max returns the maximum value of a and b.
+func max[T ordered](a, b T) T {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+// min returns the minimum value of a and b.
+func min[T ordered](a, b T) T {
+	if a < b {
+		return a
+	}
+	return b
+}
