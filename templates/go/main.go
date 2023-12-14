37a53a3545b608f28371be9443fb61c5ca98f51c
Kazumasa Hirata <k_hirata@openwork.co.jp>
2023-12-14 20:50:14 +0900

    Update templates/go

--- templates/go/main.go
+++ templates/go/main.go
@@ -8,12 +8,12 @@ import (
 	"strconv"
 )
 
-func Solve(r *scanner, w *writer) {
+func Solve(r *Scanner, w *Writer) {
 	w.Println(r.Int())
 }
 
 func main() {
-	r, w := newScanner(os.Stdin, maxBufferSize), newWriter(os.Stdout)
+	r, w := NewScanner(os.Stdin, maxBufferSize), NewWriter(os.Stdout)
 	defer w.Flush()
 	Solve(r, w)
 }
@@ -124,59 +124,60 @@ func gcd(a, b int) int {
 }
 
 // lcm returns the least common multiple of a and b.
-func lcm(a, b int) int {
-	return a * b / gcd(a, b)
-}
+func lcm(a, b int) int { return a * b / gcd(a, b) }
 
 const maxBufferSize = 1 * 1024 * 1024
 
-type scanner struct{ sc *bufio.Scanner }
+type Scanner struct{ sc *bufio.Scanner }
 
-func newScanner(r io.Reader, size int) *scanner {
+func NewScanner(r io.Reader, size int) *Scanner {
 	sc := bufio.NewScanner(r)
 	sc.Buffer(make([]byte, size), size)
 	sc.Split(bufio.ScanWords)
-	return &scanner{sc}
+	return &Scanner{sc}
 }
-func (s *scanner) scan() bool       { return s.sc.Scan() }
-func (s *scanner) text() string     { return s.sc.Text() }
-func (s *scanner) String() string   { s.scan(); return s.text() }
-func (s *scanner) Int() int         { return atoi(s.String()) }
-func (s *scanner) Float64() float64 { return atof(s.String()) }
 
-type writer struct{ bf *bufio.Writer }
+func (s *Scanner) scan() bool { return s.sc.Scan() }
 
-func newWriter(w io.Writer) *writer        { return &writer{bufio.NewWriter(w)} }
-func (w *writer) Print(a ...interface{})   { fmt.Fprint(w.bf, a...) }
-func (w *writer) Println(a ...interface{}) { fmt.Fprintln(w.bf, a...) }
-func (w *writer) Flush()                   { w.bf.Flush() }
+func (s *Scanner) text() string { return s.sc.Text() }
 
-// unionFind is a disjoint-set data structure.
-type unionFind struct {
+func (s *Scanner) String() string { s.scan(); return s.text() }
+
+func (s *Scanner) Int() int { return atoi(s.String()) }
+
+func (s *Scanner) Float64() float64 { return atof(s.String()) }
+
+type Writer struct{ bf *bufio.Writer }
+
+func NewWriter(w io.Writer) *Writer { return &Writer{bufio.NewWriter(w)} }
+
+func (w *Writer) Print(a ...interface{}) { fmt.Fprint(w.bf, a...) }
+
+func (w *Writer) Println(a ...interface{}) { fmt.Fprintln(w.bf, a...) }
+
+func (w *Writer) Flush() { w.bf.Flush() }
+
+// UnionFind is a disjoint-set data structure.
+type UnionFind struct {
 	parent []int // parent[i] = parent of i
 	size   []int // size[i] = number of elements in subtree rooted at i
 }
 
-// newUnionFind creates a new union-find data structure with n elements.
-func newUnionFind(n int) *unionFind {
-	u := &unionFind{
-		parent: make([]int, n),
-		size:   make([]int, n),
-	}
+// NewUnionFind creates a new union-find data structure with n elements.
+func NewUnionFind(n int) *UnionFind {
+	u := &UnionFind{parent: make([]int, n), size: make([]int, n)}
 	for i := 0; i < n; i++ {
-		u.parent[i] = -1
-		u.size[i] = 1
+		u.parent[i], u.size[i] = -1, 1
 	}
 	return u
 }
 
 // Union merges the components that elements x and y belong to.
-func (u *unionFind) Union(x, y int) bool {
+func (u *UnionFind) Union(x, y int) bool {
 	xRoot, yRoot := u.Find(x), u.Find(y)
 	if xRoot == yRoot {
 		return false
 	}
-
 	// Use union by size heuristic.
 	// Merge smaller component into the larger one.
 	if u.size[xRoot] < u.size[yRoot] {
@@ -188,23 +189,18 @@ func (u *unionFind) Union(x, y int) bool {
 }
 
 // Find returns the root of the component that element x belongs to.
-func (u *unionFind) Find(x int) int {
+func (u *UnionFind) Find(x int) int {
 	// x is the root of the tree
 	if u.parent[x] == -1 {
 		return x
 	}
-
 	// Use path compression heuristic.
 	u.parent[x] = u.Find(u.parent[x])
 	return u.parent[x]
 }
 
 // IsSame returns true if elements x and y belong to the same component.
-func (u *unionFind) IsSame(x, y int) bool {
-	return u.Find(x) == u.Find(y)
-}
+func (u *UnionFind) IsSame(x, y int) bool { return u.Find(x) == u.Find(y) }
 
 // Size returns the size of the component that element x belongs to.
-func (u *unionFind) Size(x int) int {
-	return u.size[u.Find(x)]
-}
+func (u *UnionFind) Size(x int) int { return u.size[u.Find(x)] }
