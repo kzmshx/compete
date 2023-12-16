c1498930dd0b08e2a84adbe68220ffd4f78ffb59
Kazumasa Hirata <k_hirata@openwork.co.jp>
2023-12-16 16:44:56 +0900

    https://atcoder.jp/contests/abc033/tasks/abc033_c

---
+++ go/atcoder/abc033/abc033_c/v1/go.mod
@@ -0,0 +1,3 @@
+module github.com/kzmshx/compete/go/atcoder/abc033/abc033_c/v1
+
+go 1.21.5
---
+++ go/atcoder/abc033/abc033_c/v1/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+func Solve(r *Scanner, w *Writer) {
+	S := r.String()
+	ans := 0
+	for _, s := range strings.Split(S, "+") {
+		if !strings.Contains(s, "0") {
+			ans++
+		}
+	}
+	w.Println(ans)
+}
+
+func main() {
+	r, w := NewScanner(os.Stdin, maxBufferSize), NewWriter(os.Stdout)
+	defer w.Flush()
+	Solve(r, w)
+}
+
+const maxBufferSize = 1 * 1024 * 1024
+
+type Scanner struct{ sc *bufio.Scanner }
+
+func NewScanner(r io.Reader, size int) *Scanner {
+	sc := bufio.NewScanner(r)
+	sc.Buffer(make([]byte, size), size)
+	sc.Split(bufio.ScanWords)
+	return &Scanner{sc}
+}
+
+func (s *Scanner) scan() bool { return s.sc.Scan() }
+
+func (s *Scanner) text() string { return s.sc.Text() }
+
+func (s *Scanner) String() string { s.scan(); return s.text() }
+
+type Writer struct{ bf *bufio.Writer }
+
+func NewWriter(w io.Writer) *Writer { return &Writer{bufio.NewWriter(w)} }
+
+func (w *Writer) Println(a ...interface{}) { fmt.Fprintln(w.bf, a...) }
+
+func (w *Writer) Flush() { w.bf.Flush() }
+
+type UnionFind struct {
+	parent []int
+	size   []int
+}
