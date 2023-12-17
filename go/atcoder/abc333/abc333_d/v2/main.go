6a48d474344fcf57d5e52f670355a9220bbe0145
Kazumasa Hirata <k_hirata@openwork.co.jp>
2023-12-17 20:34:20 +0900

    https://atcoder.jp/contests/abc333/tasks/abc333_d

--- go/atcoder/abc333/abc333_d/v2/main.go
+++ go/atcoder/abc333/abc333_d/v2/main.go
@@ -34,25 +34,15 @@ func main() {
 	Solve(r, w)
 }
 
-type signed interface {
-	~int | ~int8 | ~int16 | ~int32 | ~int64
-}
+type signed interface{ ~int | ~int32 | ~int64 }
 
-type unsigned interface {
-	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr
-}
+type unsigned interface{ ~uint | ~uint32 | ~uint64 }
 
-type integer interface {
-	signed | unsigned
-}
+type integer interface{ signed | unsigned }
 
-type float interface {
-	~float32 | ~float64
-}
+type float interface{ ~float32 | ~float64 }
 
-type ordered interface {
-	integer | float | ~string
-}
+type ordered interface{ integer | float | ~string }
 
 func unwrap[T any](value T, err error) T {
 	if err != nil {
