f08412801954573598e6a60e220c1b6c5fe6b3de
Kazumasa Hirata <k_hirata@openwork.co.jp>
2023-12-18 01:54:56 +0900

    https://atcoder.jp/contests/abc188/tasks/abc188_a

---
+++ go/atcoder/abc188/abc188_a/v1/go.mod
@@ -0,0 +1,3 @@
+module github.com/kzmshx/compete/go/atcoder/abc188/abc188_a/v1
+
+go 1.21.5
---
+++ go/atcoder/abc188/abc188_a/v1/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+func Solve(r *Scanner, w *Writer) {
+	if X, Y := MinMax(r.Int(), r.Int()); X+3 > Y {
+		w.Println("Yes")
+	} else {
+		w.Println("No")
+	}
+}
+
+func main() {
+	r, w := NewScanner(os.Stdin, maxBufferSize), NewWriter(os.Stdout)
+	defer w.Flush()
+	Solve(r, w)
+}
+
+type signed interface{ ~int | ~int32 | ~int64 }
+
+type unsigned interface{ ~uint | ~uint32 | ~uint64 }
+
+type integer interface{ signed | unsigned }
+
+type float interface{ ~float32 | ~float64 }
+
+type ordered interface{ integer | float | ~string }
+
+func Unwrap[T any](value T, err error) T {
+	if err != nil {
+		panic(err)
+	}
+	return value
+}
+
+func Atoi(s string) int { return Unwrap(strconv.Atoi(s)) }
+
+func MinMax[T ordered](a, b T) (T, T) {
+	if a > b {
+		return b, a
+	}
+	return a, b
+}
+
+const maxBufferSize = 1 * 1024 * 1024
+
+type Scanner struct{ sc *bufio.Scanner }
+
+func NewScanner(r io.Reader, size int) *Scanner {
+	sc := bufio.NewScanner(r)
+	sc.Buffer(make([]byte, size), size)
+	sc.Split(bufio.ScanWords)
+	return &Scanner{sc}
+}
+
+func (s *Scanner) scan() bool { return s.sc.Scan() }
+
+func (s *Scanner) text() string { return s.sc.Text() }
+
+func (s *Scanner) String() string { s.scan(); return s.text() }
+
+func (s *Scanner) Int() int { return Atoi(s.String()) }
+
+type Writer struct{ bf *bufio.Writer }
+
+func NewWriter(w io.Writer) *Writer { return &Writer{bufio.NewWriter(w)} }
+
+func (w *Writer) Println(a ...interface{}) { fmt.Fprintln(w.bf, a...) }
+
+func (w *Writer) Flush() { w.bf.Flush() }
+
+type UnionFind struct {
+	parent []int
+	size   []int
+}
