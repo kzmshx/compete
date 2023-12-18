75d37c3cd65865d6592f68592ae144cacba25318
Kazumasa Hirata <k_hirata@openwork.co.jp>
2023-12-19 01:29:44 +0900

    https://atcoder.jp/contests/abc186/tasks/abc186_c

---
+++ go/atcoder/abc186/abc186_c/v2/go.mod
@@ -0,0 +1,3 @@
+module github.com/kzmshx/compete/go/atcoder/abc186/abc186_c/v2
+
+go 1.21.5
---
+++ go/atcoder/abc186/abc186_c/v2/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+func Solve(r *Scanner, w *Writer) {
+	N := r.Int()
+	a := 0
+	for i := 1; i <= N; i++ {
+		if All(Digits(i, 10), func(d int) bool { return d != 7 }) && All(Digits(i, 8), func(d int) bool { return d != 7 }) {
+			a++
+		}
+	}
+	w.Println(a)
+}
+
+func main() {
+	r, w := NewScanner(os.Stdin, maxBufferSize), NewWriter(os.Stdout)
+	defer w.Flush()
+	Solve(r, w)
+}
+
+type signed interface{ ~int | ~int32 | ~int64 }
+
+type unsigned interface{ ~uint | ~uint32 | ~uint64 }
+
+type integer interface{ signed | unsigned }
+
+func Unwrap[T any](value T, err error) T {
+	if err != nil {
+		panic(err)
+	}
+	return value
+}
+
+func Atoi(s string) int { return Unwrap(strconv.Atoi(s)) }
+
+func Digits[T integer](n T, base T) (r []T) {
+	for n > 0 {
+		r = append(r, n%base)
+		n /= base
+	}
+	return r
+}
+
+func All[T any](s []T, f func(T) bool) bool {
+	for _, v := range s {
+		if !f(v) {
+			return false
+		}
+	}
+	return true
+}
+
+const maxBufferSize = 1 * 1024 * 1024
+
+type Scanner struct{ sc *bufio.Scanner }
+
+func NewScanner(r io.Reader, size int) *Scanner {
+	sc := bufio.NewScanner(r)
+	sc.Buffer(make([]byte, size), size)
+	sc.Split(bufio.ScanWords)
+	return &Scanner{sc}
+}
+
+func (s *Scanner) scan() bool { return s.sc.Scan() }
+
+func (s *Scanner) text() string { return s.sc.Text() }
+
+func (s *Scanner) String() string { s.scan(); return s.text() }
+
+func (s *Scanner) Int() int { return Atoi(s.String()) }
+
+type Writer struct{ bf *bufio.Writer }
+
+func NewWriter(w io.Writer) *Writer { return &Writer{bufio.NewWriter(w)} }
+
+func (w *Writer) Println(a ...interface{}) { fmt.Fprintln(w.bf, a...) }
+
+func (w *Writer) Flush() { w.bf.Flush() }
+
+type UnionFind struct {
+	parent []int
+	size   []int
+}
