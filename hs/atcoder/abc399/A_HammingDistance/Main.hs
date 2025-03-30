4f1be6e42011afd49369581d20c5a3df87a8687e
Kazumasa Hirata <k_hirata@openwork.co.jp>
2025-03-30 22:44:59 +0900

    chore(hs/atcoder): abc399_a

---
+++ hs/atcoder/abc399/A_HammingDistance/Main.hs
@@ -0,0 +1,12 @@
+-- Pipe operator
+(|>) :: a -> (a -> b) -> b
+x |> f = f x
+
+infixl 1 |>
+
+main :: IO ()
+main = do
+  _ :: Int <- readLn
+  s <- getLine
+  t <- getLine
+  zip s t |> filter (uncurry (/=)) |> length |> print
