d2236f422128807ac637f125e31d5428ed68d548
Kazumasa Hirata <50480499+kzmshx@users.noreply.github.com>
2025-08-30 22:46:31 +0900

    feat(go/atcoder-cli/abc421/c): implement C - Alternated problem with input handling, output formatting, and test cases; add documentation

---
+++ go/atcoder-cli/abc421/c/main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+func Solve(r *Scanner, w *Writer) {
+	_, s := r.Int(), r.String()
+	t := 0
+	costOdd, costEven := 0, 0
+	for i, c := range s {
+		if c == 'B' {
+			continue
+		}
+		costOdd, costEven = costOdd+Abs(i-t), costEven+Abs(i-(t+1))
+		t += 2
+	}
+	w.Println(Min(costOdd, costEven))
+}
+
+func main() {
+	r, w := NewScanner(os.Stdin, MaxBufferSize), NewWriter(os.Stdout)
+	defer w.Flush()
+	Solve(r, w)
+}
+
+const MaxBufferSize = 1 * 1024 * 1024
+
+type Scanner struct{ sc *bufio.Scanner }
+
+func NewScanner(r io.Reader, size int) *Scanner {
+	sc := bufio.NewScanner(r)
+	sc.Buffer(make([]byte, size), size)
+	sc.Split(bufio.ScanWords)
+	return &Scanner{sc}
+}
+func (s *Scanner) scan() bool     { return s.sc.Scan() }
+func (s *Scanner) text() string   { return s.sc.Text() }
+func (s *Scanner) String() string { s.scan(); return s.text() }
+func (s *Scanner) Int() int       { return Atoi(s.String()) }
+
+type Writer struct{ bf *bufio.Writer }
+
+func NewWriter(w io.Writer) *Writer { return &Writer{bufio.NewWriter(w)} }
+func (w *Writer) Println(a ...any)  { fmt.Fprintln(w.bf, a...) }
+func (w *Writer) Flush()            { w.bf.Flush() }
+
+type Signed interface{ ~int | ~int32 | ~int64 }
+type Unsigned interface{ ~uint | ~uint32 | ~uint64 }
+type Integer interface{ Signed | Unsigned }
+type Float interface{ ~float32 | ~float64 }
+type Actual interface{ Integer | Float }
+type Imaginary interface{ ~complex64 | ~complex128 }
+type Ordered interface{ Integer | Float | ~string }
+type Addable interface {
+	Integer | Float | Imaginary | ~string
+}
+
+func Atoi(s string) int { return Unwrap(strconv.Atoi(s)) }
+
+func Min[T Ordered](a, b T) T {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func Abs[T Actual](x T) T {
+	if x < T(0) {
+		return -x
+	}
+	return x
+}
+
+type UnionFind struct {
+	parent []int
+	size   []int
+}
+
+type priorityQueueItem[T any, P Ordered] struct {
+	value    T
+	priority P
+}
+
+type PriorityQueue[T any, P Ordered] struct {
+	items      []*priorityQueueItem[T, P]
+	itemCount  uint
+	comparator func(lhs, rhs P) bool
+}
+
+type Diff[T Actual] struct{ delta []T }
+
+func Unwrap[T any](value T, err error) T {
+	if err != nil {
+		panic(err)
+	}
+	return value
+}
