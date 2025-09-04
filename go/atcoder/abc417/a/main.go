62c5e0fa6482da8643b94a47a28aa0e5779f25d1
Kazumasa Hirata <50480499+kzmshx@users.noreply.github.com>
2025-09-04 14:42:41 +0900

    feat(go/atcoder/abc417/a): add solution for `A Substring` with input/output handling, test cases, and Makefile for testing

---
+++ go/atcoder/abc417/a/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+func Solve(r *Reader, w *Writer) {
+	_, a, b := r.Int(), r.Int(), r.Int()
+	s := r.String()
+	w.Println(s[a : len(s)-b])
+}
+
+func main() {
+	r, w := NewReader(os.Stdin, 1*1024*1024), NewWriter(os.Stdout)
+	defer w.Flush()
+	Solve(r, w)
+}
+
+type Sgn interface{ ~int | ~int32 | ~int64 }
+type Uns interface{ ~uint | ~uint32 | ~uint64 }
+type Int interface{ Sgn | Uns }
+type Float interface{ ~float32 | ~float64 }
+type Num interface{ Int | Float }
+type Ord interface{ Int | Float | ~string }
+
+func Unwrap[T any](value T, err error) T {
+	if err != nil {
+		panic(err)
+	}
+	return value
+}
+func Atoi(s string) int { return Unwrap(strconv.Atoi(s)) }
+
+type Reader struct{ sc *bufio.Scanner }
+
+func NewReader(r io.Reader, size int) *Reader {
+	sc := bufio.NewScanner(r)
+	sc.Buffer(make([]byte, size), size)
+	sc.Split(bufio.ScanWords)
+	return &Reader{sc}
+}
+func (r *Reader) Int() int       { return Atoi(r.String()) }
+func (r *Reader) String() string { r.sc.Scan(); return r.sc.Text() }
+
+type Writer struct{ bf *bufio.Writer }
+
+func NewWriter(w io.Writer) *Writer { return &Writer{bufio.NewWriter(w)} }
+func (w *Writer) Println(a ...any)  { fmt.Fprintln(w.bf, a...) }
+func (w *Writer) Flush()            { w.bf.Flush() }
